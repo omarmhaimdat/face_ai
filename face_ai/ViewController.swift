a444ee35af51f18c4393d8749814822de44754c4
Omar MHAIMDAT <omarmhaimdat@gmail.com>
2020-02-29 20:06:04 +0100

    Initial Commit

---
+++ face_ai/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  face_ai
+//
+//  Created by M'haimdat omar on 29-02-2020.
+//  Copyright © 2020 M'haimdat omar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
